2c1ef670eff82987f618c2e3f896ecbf75334f41
Artem Bohomolov <artembohomolov@cloud.upwork.com>
2020-02-16 22:40:01 +0200

    styling wip

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -8,10 +8,22 @@ const adjustStrSize = (str, length = 2, filler = "0") => {
   return str;
 };
 
+const CountdownWrapper = styled.div`
+  font-family: "Share Tech Mono", monospace;
+  color: #ffffff;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: #daf6ff;
+  text-shadow: 0 0 20px rgba(10, 175, 230, 1), 0 0 20px rgba(10, 175, 230, 0);
+`;
+
 const CountdownTimer = styled.div`
-  display: inline-block;
-  font-size: 30px;
-  font-weight: 900;
+  letter-spacing: 0.05em;
+  font-size: 80px;
+  padding: 5px 0;
 `;
 
 const ToggleTimerButton = styled.button`
@@ -33,7 +45,7 @@ export default function({ remainingTime = 0, toggleTimer, isPaused = false }) {
   const seconds = adjustStrSize(`${remainingTime % 60}`);
 
   return (
-    <div>
+    <CountdownWrapper>
       <CountdownTimer>
         {minutes}:{seconds}
       </CountdownTimer>
@@ -43,6 +55,6 @@ export default function({ remainingTime = 0, toggleTimer, isPaused = false }) {
           {isPaused ? <PlayIcon /> : <PauseIcon />}
         </ToggleTimerButton>
       )}
-    </div>
+    </CountdownWrapper>
   );
 }
